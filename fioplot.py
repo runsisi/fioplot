b122b81f7b149a7fc4e38d383b86edbaed7dc23e
runsisi <runsisi@hust.edu.cn>
2021-03-21 21:35:22 +0800

    fix plot
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- fioplot.py
+++ fioplot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
 import glob
 import re
 import os
@@ -42,8 +43,11 @@ def plot(log, out_dir):
     if not data:
         return
 
+    data.insert(0, {'time': 0, 'value': 0})
+
     df = pd.DataFrame(data)
-    df['time'] = pd.to_datetime(df.time, unit='ms').dt.time
+    dt = pd.to_datetime(df.time, unit='ms')
+    df['time'] = dt.dt.time
     df.set_index('time', inplace=True)
     df = df.groupby(['time'])['value'].sum().reset_index()
 
@@ -52,7 +56,23 @@ def plot(log, out_dir):
     if log_type in ['lat', 'slat', 'clat']:
         df['value'] = df.apply(lambda x: x['value'] / 1000000, axis=1)
 
-    df.plot(kind='line', x='time', y='value', title=fn, legend=False)
+    # Dataframe Visualization with Pandas Plot
+    # https://kanoki.org/2019/09/16/dataframe-visualization-with-pandas-plot/
+
+    xmax = df['time'].max()
+    ymax = int(df['value'].max())
+
+    def santize(t):
+        return datetime.time(t.hour, t.minute, t.second)
+
+    time_ranges = pd.date_range(start=dt.min(), end=dt.max(), freq='30S').time
+    value_ranges = [0, 10, 20, 50]
+
+    df.plot(kind='line', x='time', y='value', title=fn, legend=False, figsize=(15, 9),
+            xlim=(0, xmax), ylim=(0, ymax),
+            xticks=list(map(santize, time_ranges)),
+            yticks=value_ranges.extend([w*100 for w in range(ymax)]),
+            rot=30)
 
     # save plot to image
     fn = os.path.splitext(fn)[0]
